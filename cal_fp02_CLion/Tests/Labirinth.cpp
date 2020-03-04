4eb66f095cb746a76bdb1bef0466339278a11748
Nunation <oliveiranunopt@gmail.com>
2020-03-04 09:56:41 +0000

    Did labyrinth algo

--- cal_fp02_CLion/Tests/Labirinth.cpp
+++ cal_fp02_CLion/Tests/Labirinth.cpp
@@ -14,6 +14,7 @@ Labirinth::Labirinth(int values[10][10])
 	for (int i = 0; i < 10; i++)
 		for (int j = 0; j < 10; j++)
 			labirinth[i][j] = values[i][j];
+    initializeVisited();
 }
 
 
@@ -41,7 +42,19 @@ void  Labirinth::printLabirinth()
 
 bool Labirinth::findGoal(int x, int y)
 {
-	return false;
+	if(visited[x][y] || (labirinth[x][y] == 0)){return false;}
+	else{visited[x][y] = true;}
+
+    if(labirinth[x][y]==2){return true;}
+
+    else{
+        if(findGoal(x+1,y)){return true;}
+        if(findGoal(x-1,y)){return true;}
+        if(findGoal(x,y+1)){return true;}
+        if(findGoal(x,y-1)){return true;}
+    }
+
+    return false;
 }
 
 
